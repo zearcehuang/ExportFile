5b88dfd9dc3ef09bb140b746b0722862a3ed8e8d
zearcehuang <zearcehuang@gmail.com>
2017-06-13 10:57:51 +0800

    修正log4net紀錄

--- export sybase/Program.cs
+++ export sybase/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace export_sybase
@@ -14,6 +11,7 @@ namespace export_sybase
         [STAThread]
         static void Main()
         {
+            log4net.Config.XmlConfigurator.Configure();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
